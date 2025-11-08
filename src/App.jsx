adf847cfc205511f601459859e2bdd6f28b30cea
Victoria Vardina <vsvetoslava@gmail.com>
2025-11-08 16:55:18 +0100

    putting the two profile pages into one and renaming

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Header from "./components/header/Header.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import Home from "./pages/Home.jsx";
-import UserProfile from "./pages/UserProfile";
-import OtherUserProfile from "./pages/OtherUserProfile";
+import UserProfile from "./pages/UserProfile"; 
 import BumpSent from "./pages/BumpSent.jsx";
 import BumpReceived from "./pages/BumpReceived.jsx";
 
@@ -18,7 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/user-profile" element={<UserProfile />} />
-          <Route path="/user/:userId" element={<OtherUserProfile />} />  
+          <Route path="/user/:userId" element={<UserProfile />} />  
           <Route path="/bump-sent" element={<BumpSent />} />
           <Route path="/bump-received" element={<BumpReceived />} />
         </Routes>
