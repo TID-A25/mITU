962dc31ef2a38f06bcd4de20b6072de692847f74
Caroline Bjørner <carolinebjorner@gmail.com>
2025-11-10 21:30:43 +0100

    replace footer buttons with icons

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,6 +2,9 @@ import { Link } from "react-router-dom";
 import React from "react";
 import "./Footer.css";
 import "../buttons/Buttons.css";
+import bell from "../../assets/images/icons/Bell.svg";
+import home from "../../assets/images/icons/Home.svg";
+import user from "../../assets/images/icons/User.svg";
 
 /**
  * Footer - bottom navigation bar
@@ -10,13 +13,19 @@ export default function Footer() {
   return (
     <footer className="bottom-nav">
       <Link to="">
-        <button className="button button--small">🔔</button>
+        <button className="button button--small" aria-label="Notifications">
+          <img src={bell} alt="" className="icon" />
+        </button>
       </Link>
       <Link to="/">
-        <button className="button button--small">🏠</button>
+        <button className="button button--small" aria-label="Home">
+          <img src={home} alt="" className="icon" />
+        </button>
       </Link>
       <Link to="/user-profile">
-        <button className="button button--small">👤</button>
+        <button className="button button--small" aria-label="Profile">
+          <img src={user} alt="" className="icon" />
+        </button>
       </Link>
     </footer>
   );
