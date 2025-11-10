a442b38d41786612801cab0dbc1515dc13f87d54
Victoria Vardina <vsvetoslava@gmail.com>
2025-11-10 16:25:29 +0100

    fixing parse path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 //Setting up Parse
 // ** importing Parse as configured in vite.config.js:
-import "./parseConfig.js";
+import "./constants/parseConfig.js"
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Header from "./components/header/Header.jsx";
 import Footer from "./components/footer/Footer.jsx";
