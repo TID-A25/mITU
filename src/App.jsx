90f8eefbd43320e379f0018fee0f3dec2fc7d236
Caroline Bjørner <carolinebjorner@gmail.com>
2025-11-23 15:28:24 +0100

    create notification pages and components with mock data for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 //Setting up Parse
 // ** importing Parse as configured in vite.config.js:
-import "./constants/parseConfig.js"
+import "./constants/parseConfig.js";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Header from "./components/header/Header.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import Home from "./pages/Home.jsx";
-import UserProfile from "./pages/UserProfile"; 
+import UserProfile from "./pages/UserProfile";
 import BumpSent from "./pages/BumpSent.jsx";
 import BumpReceived from "./pages/BumpReceived.jsx";
+import Notifications from "./pages/Notifications.jsx";
 
 function App() {
   return (
@@ -17,9 +18,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/user-profile" element={<UserProfile />} />
-          <Route path="/user/:userId" element={<UserProfile />} />  
+          <Route path="/user/:userId" element={<UserProfile />} />
           <Route path="/bump-sent/:otherUserId" element={<BumpSent />} />
           <Route path="/bump-received" element={<BumpReceived />} />
+          <Route path="/notifications" element={<Notifications />} />
         </Routes>
         <Footer />
       </div>
