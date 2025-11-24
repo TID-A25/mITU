4b841a79e118316eed6887d87ed5d2a657286c5b
Jakob Thiberg <jakob.t.thiberg@gmail.com>
2025-11-24 15:16:06 +0100

    Fixed settings not showing up error

--- src/components/profileHeader/ProfileHeader.jsx
+++ src/components/profileHeader/ProfileHeader.jsx
@@ -1,42 +1,35 @@
+// ProfileHeader component - displays cover photo and profile picture for user profiles
 import React from "react";
 import { useNavigate } from "react-router-dom";
 // asset paths: this file lives at src/components/profile, so go up two levels to src/
 import coverPhoto from "../../assets/images/profiles/Coverpicture.jpg";
+import settingsIcon from "../../assets/images/icons/Settings.svg";
 import "./ProfileHeader.css";
-import "../buttons/Buttons.css";
 
-export default function ProfileHeader({
-  profilePicture
-}) {
+export default function ProfileHeader({ profilePicture, isOwnProfile = false }) {
   const navigate = useNavigate();
+  
   return (
     <div className="header-wrap">
+      {/* Cover photo banner */}
       <div className="cover-photo-header">
         <img src={coverPhoto} alt="Cover" />
       </div>
 
+      {/* Profile picture overlapping the cover photo */}
       <div className="profile-picture-header">
         <img src={profilePicture} alt="Profile" className="profile-img" />
-
-        {showSettings && (
+        
+        {/* Settings icon - only shown on own profile */}
+        {isOwnProfile && (
           <img
             src={settingsIcon}
             className="settings-icon"
-            alt="Settings icon"
+            alt="Settings"
             role="button"
             tabIndex={0}
             onClick={() => navigate("/edit-profile")}
-          />
-        )}
-
-        {!showSettings && (
-          // calls the onBump prop when user clicks
-          <Buttons
-            mode="single"
-            size="small"
-            color="teal"
-            label="Bump"
-            onClick={onBump}
+            onKeyDown={(e) => e.key === "Enter" && navigate("/edit-profile")}
           />
         )}
       </div>
