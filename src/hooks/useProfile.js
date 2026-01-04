206f1f466d57612b0cdd211f7e34b3d5a691b67a
Victoria Vardina <vsvetoslava@gmail.com>
2026-01-04 15:18:58 +0100

    comments for usenotifications

--- src/hooks/useProfile.js
+++ src/hooks/useProfile.js
@@ -2,8 +2,10 @@ import { useEffect, useState, useCallback } from 'react';
 import { fetchProfileById } from '../services/parseQueries';
 import { CURRENT_USER_ID, initializeCurrentUser } from "../constants/currentUser";
 
+//if autoRefresh is true, it loads right away, otherwise it waits for the refreshIndex to change
 export default function useProfile(profileId, { autoRefresh = true } = {}) {
 	const [profile, setProfile] = useState(null);
+	//if profileid is truthy, set initial loading to true
 	const [loading, setLoading] = useState(Boolean(profileId && autoRefresh));
 	const [error, setError] = useState(null);
 	const [refreshIndex, setRefreshIndex] = useState(0);
@@ -11,7 +13,8 @@ export default function useProfile(profileId, { autoRefresh = true } = {}) {
 	//user switching state tracks which user is currently selected in dropdown
 	const [currentDemoUser, setCurrentDemoUser] = useState(CURRENT_USER_ID);
 
-	//updates currentdemouser to the selected user and triggers refresh
+	//For the userswitcher.jsx
+	// Updates currentdemouser to the selected user and triggers refresh.
     const handleUserChange = useCallback((newUserId) => {
         setCurrentDemoUser(newUserId);
         refresh();
@@ -57,8 +60,11 @@ export default function useProfile(profileId, { autoRefresh = true } = {}) {
 
 	const refresh = useCallback(() => setRefreshIndex((i) => i + 1), []);
 
+	/* After a user has edited something in editprofile, 
+	it gets saved locally to update the UI immediately 
+	before you save it to the server*/
 	const updateLocal = useCallback((patch) => setProfile((p) => (p ? { ...p, ...patch } : p)), []);
-
+    //                                                               ↑ if profile exists  ↑ if profile is null
     return { 
         profile, 
         loading, 
